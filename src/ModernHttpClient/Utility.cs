45dfc661ad00faa72dcae703971c67db480d800c
Ankit Choudhari <ankitch@microsoft.com>
2016-04-20 12:46:27 -0700

    update comments to new one

--- src/ModernHttpClient/Utility.cs
+++ src/ModernHttpClient/Utility.cs
@@ -8,18 +8,16 @@ namespace ModernHttpClient
     {
         public static bool MatchHostnameToPattern(string hostname, string pattern)
         {
-
-            // check if its a regular expression
+            
             string[] splitPattern = pattern.Split('*');
 
-            //it is not a pattern lets just do a case-insensitive check.
+         
             if (splitPattern.Length == 1)
             {
                 bool ret =  (String.Compare(hostname, pattern, true, CultureInfo.InvariantCulture) == 0);
                 return ret;
             }
-
-            // wild card character may be used as the left most name in the certificate unless this is the last char
+            
             if (splitPattern.Length == 2)
             {
                 if (!splitPattern[1].StartsWith(".") && String.IsNullOrEmpty(splitPattern[1]))
@@ -29,7 +27,6 @@ namespace ModernHttpClient
 
             }
 
-            // can contain only one wild card
             if (splitPattern.Length == 3)
             {
                 return false;
